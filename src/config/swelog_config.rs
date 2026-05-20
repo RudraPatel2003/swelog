96877a33926243259209a137610571f7ebd3e30d
Rudra Patel <patelrudra2003@gmail.com>
2026-05-20 15:46:05 -0500

    feat: Create init command

---
+++ src/config/swelog_config.rs
@@ -0,0 +1,30 @@
+use serde::{Deserialize, Serialize};
+use std::path::{Path, PathBuf};
+
+const APP_NAME: &str = "swelog";
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub struct SwelogConfig {
+    pub obsidian_vault_path: PathBuf,
+    pub daily_log_folder: PathBuf,
+    pub weekly_log_folder: PathBuf,
+    pub monthly_log_folder: PathBuf,
+    pub quarterly_log_folder: PathBuf,
+    pub yearly_log_folder: PathBuf,
+}
+
+impl SwelogConfig {
+    pub fn get_default_config(vault_directory: &Path) -> Self {
+        let vault_path = vault_directory.join(APP_NAME);
+
+        Self {
+            obsidian_vault_path: vault_path,
+            daily_log_folder: PathBuf::from("swelog/daily"),
+            weekly_log_folder: PathBuf::from("swelog/weekly"),
+            monthly_log_folder: PathBuf::from("swelog/monthly"),
+            quarterly_log_folder: PathBuf::from("swelog/quarterly"),
+            yearly_log_folder: PathBuf::from("swelog/yearly"),
+        }
+    }
+}
